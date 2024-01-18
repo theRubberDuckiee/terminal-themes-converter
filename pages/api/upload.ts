7eb83bca0677c726ef9150d28a139f4959609d30
Jess <jessica.wang360@gmail.com>
2024-01-17 22:34:41 -0800

    rewrote python script in javascript

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -2,44 +2,167 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const path = require('path');
 import fs from 'fs';
 const { exec } = require('child_process');
+import * as xml2js from 'xml2js';
+const yaml = require('js-yaml');
+
+function convertRGBToHex(rgbArray: number[]): string {
+  // Convert float values to integers
+  const rgbIntegers = rgbArray.map(component => Math.round(component * 255));
+
+  // Convert integers to hex format
+  const hexString = rgbIntegers
+      .map(component =>
+        Number(component).toString(16).padStart(2, '0'))
+        .join('');
+
+  return `#${hexString}`;
+}
  
 export default async function upload(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  console.log('can you see this?')
   const body = JSON.parse(req.body)
   try {
     if (!req.body) {
         return res.status(400).send('No file uploaded.');
     }
     const fileNameWithExtension = body.name;
-    console.log('fileNameWithExtension: ', fileNameWithExtension)
     const lastDotIndex = fileNameWithExtension.lastIndexOf('.');
     const fileNameWithoutExtension = fileNameWithExtension.substring(0, lastDotIndex);
 
-    // Save the uploaded file temporarily
-    const filePath = path.join(process.cwd(), '/uploads', fileNameWithoutExtension + '.txt');
-    console.log('filePath: ', filePath)
-    try {
-      await fs.promises.writeFile(filePath, body.text);
-      console.log('File written successfully.');
-    } catch (error) {
-      console.error('Error writing file:', error);
-    }
-    console.log("done")
-    // Call your Python script with the uploaded file
-    const pythonScript = path.join(process.cwd(), '/convert-iterm2-to-warp.py');
-    console.log('pythonScript', pythonScript)
-    exec(`python ${pythonScript} ${filePath}`, (error, stdout, stderr) => {
-        if (error) {
-            console.error('Error running Python script:', error);
-            return res.status(500).send('Error running Python script.');
-        }
+    const newText = {
+      accent: '',
+      details: 'darker',
+      foreground: '',
+      background: '',
+      terminal_colors: {
+          bright: {
+            black: '',
+            blue: '',
+            cyan: '',
+            green: '',
+            magenta: '',
+            red: '',
+            white: '',
+            yellow: '',
+          },
+          normal: {
+            black: '',
+            blue: '',
+            cyan: '',
+            green: '',
+            magenta: '',
+            red: '',
+            white: '',
+            yellow: '',
+
+          }
+          }
+      }
+    const xmlString = body.text
+
+    xml2js.parseString(xmlString, (err, result) => {
+      if (err) {
+        console.error('Error parsing XML:', err);
+      } else {
+        const ka = result.plist.dict[0].key
+        const bla = result.plist.dict[0].dict
+
+        for (let j=0; j<ka.length; j++) {
+          const obj = bla[j]
+          const keys = obj.key
+          const index = keys.indexOf('Color Space');
+
+          // If found, remove it using splice
+          if (index !== -1) {
+            keys.splice(index, 1);
+          }
+
+          const reals = obj.real
+          const ansi = ka[j]
+          let blue = 0
+          let green = 0
+          let red = 0
+          for (let i = 0; i < keys.length; i++) {
+            const cha = keys[i]
+            if (cha == 'Red Component') {
+              red = Number(reals[i])
+            }
+            if (cha == 'Blue Component') {
+              blue = Number(reals[i])
+            }
+            if (cha == 'Green Component') {
+              green = Number(reals[i])
+            }
+          }
+            if (ansi == "Ansi 0 Color"){
+              newText.terminal_colors.normal.black = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 1 Color") {
+              newText.terminal_colors.normal.red = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 10 Color") {
+              newText.terminal_colors.bright.green = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 11 Color") {
+              newText.terminal_colors.bright.yellow = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 12 Color") {
+              newText.terminal_colors.bright.blue = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 13 Color") {
+              newText.terminal_colors.bright.magenta = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 14 Color") {
+              newText.terminal_colors.bright.cyan = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 15 Color") {
+
+              newText.terminal_colors.bright.white = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 2 Color") {
+              newText.terminal_colors.normal.green = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 3 Color"){
+              newText.terminal_colors.normal.yellow = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 4 Color"){
+              newText.terminal_colors.normal.blue = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 5 Color"){
+              newText.terminal_colors.normal.magenta = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 6 Color"){
+              newText.terminal_colors.normal.cyan = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 7 Color"){
+              newText.terminal_colors.normal.white = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 8 Color"){
+              newText.terminal_colors.bright.black = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Ansi 9 Color"){
+              newText.terminal_colors.bright.red = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Background Color"){
+              newText.background = convertRGBToHex([red, green, blue])
+            }
+            if (ansi == "Foreground Color"){
+              newText.foreground = convertRGBToHex([red, green, blue])
+            }
+            newText.accent = newText.foreground
+      }
+      console.log(newText)
+      }
+    });
+
+    const yamlString = yaml.dump(newText);
+    
 
         const generatedFileName = fileNameWithoutExtension + '.yaml';
         const yamlFilePath = path.join(process.cwd(), '/generated', generatedFileName);
-        console.log('yamlfilepath:', yamlFilePath)
+        await fs.promises.writeFile(yamlFilePath, yamlString);
 
         // Send the download link as the response
         const downloadLink = `/api/download/${generatedFileName}`;
@@ -51,7 +174,6 @@ export default async function upload(
           downloadLink: downloadLink,
           filename: fileNameWithoutExtension,
       });
-    });
   } catch (error) {
       res.status(500).send('Error processing upload.');
   }
